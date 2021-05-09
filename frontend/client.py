95e3b01bd1f7baa6c3fcfb9dccaf2282d0adf087
Alexander Lessacher <cc69222@escpe.net>
2021-05-09 16:49:17 +0200

    Switched to from ui files to generated ui code
    pyuic6 generates code in form of a class to use
    this has many advantages to the loaded ui files.

--- frontend/client.py
+++ frontend/client.py
@@ -10,7 +10,7 @@ import os.path
 import logging
 import logging.config
 import threading
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6 import uic # .ui files and their content
 from PyQt6.QtCore import pyqtSignal as Signal, pyqtSlot as Slot # ui elements communication
 from PyQt6.QtCore import Qt
@@ -19,6 +19,8 @@ from PyQt6.QtWidgets import *
 
 from userstub import *
 from settingsstub import *
+from mainwindow import Ui_MainWindow
+from settingswindow import Ui_SettingsWindow
 
 sys.path.append('../client')
 import chat_client
@@ -26,41 +28,25 @@ import client_functions
 
 @Slot()
 def send_msg():
-  """Send message function
+    """Send message function
 
-  This function is called when the user presses Enter,
-  to send the message."""
-  r = window.userSelect.currentText()
-  t = window.InputBar.text()
-  if r == "all":
-    logging.info(f"frontend broadcasting message '{t}'")
-  else:
-    logging.info(f"frontend sending message '{t}' to '{r}'")
-  display_message(chat_client.user,r,t)
-  client_functions.text_message(chat_client.my_client.sock, t, chat_client.user, r)
-  logging.info(f"frontend calling textmessage with {chat_client.my_client.sock},{t},{chat_client.user}->{r}")
-  window.InputBar.clear()
+    This function is called when the user presses Enter,
+    to send the message."""
+    r = mainwindowui.userSelect.currentText()
+    t = mainwindowui.InputBar.text()
+    if r == "all":
+        logging.info(f"frontend broadcasting message '{t}'")
+    else:
+        logging.info(f"frontend sending message '{t}' to '{r}'")
+    display_message(chat_client.user,r,t)
+    client_functions.text_message(chat_client.my_client.sock, t, chat_client.user, r)
+    logging.info(f"frontend calling textmessage with {chat_client.my_client.sock},{t},{chat_client.user}->{r}")
+    mainwindowui.InputBar.clear()
 
 
 def display_message(sender : str, recipient : str, message : str):
-  """Displays a message on the msgList :: QListWidget """
-  window.msgList.addItem(f"{sender} -> {recipient}: {message}")
-
-
-def load_ui_file(filename):
-  """UI File Loader
-
-  Path independent loader function for QTCreators .ui files
-  """
-  script_path = os.path.realpath(__file__)
-  script_path_list = script_path.split("/")
-  script_path_list[-1] = filename
-  ui_path = "/".join(script_path_list)
-  if os.path.exists(ui_path):
-    return uic.loadUi(ui_path)
-  else:
-    logging.error(f"Cannot open {ui_path} to load {filename}")
-    sys.exit(1)
+    """Displays a message on the msgList :: QListWidget """
+    mainwindowui.msgList.addItem(f"{sender} -> {recipient}: {message}")
 
 def main_read_loop(sock):
     """Reads the socket in a loop"""
@@ -71,7 +57,7 @@ def main_read_loop(sock):
             chat_client.shutdown = True
             return
         if( type(message) == int):
-          print("got a hinig message from socket:",message)
+          logging.error(f"Got Integer from socket : {message}")
         elif message["type"] == "text":
             if message["author"] != chat_client.user:
               display_message(message['author'],message['recipient'],message['content'])
@@ -85,16 +71,20 @@ if __name__ == "__main__":
   app = QApplication(sys.argv)
   #app.setStyle('Fusion') # only Windows or Fusion
 
-  window = load_ui_file("mainwindow.ui")
-  window.InputBar.returnPressed.connect(send_msg)
-  test_user_table(window)
-  test_combo_box(window)
-  window.actionExit.triggered.connect(app.quit)
-  window.show()
+  mainwindow = QMainWindow()
+  mainwindowui = Ui_MainWindow()
+  mainwindowui.setupUi(mainwindow)
+  mainwindowui.InputBar.returnPressed.connect(send_msg)
+  test_user_table(mainwindowui)
+  test_combo_box(mainwindowui)
+  mainwindowui.actionExit.triggered.connect(app.quit)
+  mainwindow.show()
 
-  settings = load_ui_file("settingswindow.ui")
-  window.actionServer.triggered.connect(settings.show)
-  init_settings_window(settings)
+  settingswindow = QMainWindow()
+  settingswindowui = Ui_SettingsWindow()
+  settingswindowui.setupUi(settingswindow)
+  mainwindowui.actionServer.triggered.connect(settingswindow.show)
+  init_settings_window(settingswindowui)
 
   display_thread = threading.Thread(target=main_read_loop, args=(chat_client.my_client.sock,))
   display_thread.start()
